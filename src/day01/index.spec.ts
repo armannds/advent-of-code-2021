3dfe96a6c4a9d1893eb7f8ea595724b7f9d22515
Ármann Davíð Sigurðsson <armannds@gmail.com>
2021-12-01 22:42:47 +0000

    :white_check_mark:  Fix test setup

--- src/day01/index.spec.ts
+++ src/day01/index.spec.ts
@@ -3,7 +3,7 @@ import { readFileSync } from "fs";
 import path = require("path");
 
 describe("day01", () => {
-  it("Given input part1 should return 7", () => {
+  it("Given input part 1 should return 7", () => {
     const filepath = path.resolve(__dirname, "input.spec.txt");
     const file = readFileSync(filepath, "utf-8");
     const input = file.split("\n");
